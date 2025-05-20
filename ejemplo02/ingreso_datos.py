eaaa797939e4859a1a487d475f8a34316829c403
santyago0 <santyago0515@gmail.com>
2025-05-20 09:58:38 -0500

    lectura de los datos de los jugadores

--- ejemplo02/ingreso_datos.py
+++ ejemplo02/ingreso_datos.py
@@ -17,7 +17,42 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # OBTENCION DE DATOS - CLUB
-with open('data/datos_clubs.txt', 'r', encoding='utf-8') as clubs:
-    for c in clubs:
-        datos = c.strip().split(';')
-        print(datos)
+clubs = [] # Lista para almacenar los objetos de tipo Club
+with open('data/datos_clubs.txt', 'r', encoding='utf-8') as data_clubs:
+    for c in data_clubs:
+        row_clubs = c.strip().split(';') # Lectura de las lineas, seprandolas por ;
+        # Crear un objeto de tipo Club
+        # Pasarle los parametros obtenidos
+        # como todos los datos son cadenas, la fundacion la transformamos a entero
+        club = Club(nombre=row_clubs[0],deporte=row_clubs[1],fundacion=int(row_clubs[2]))
+        print(club)
+        clubs.append(club)
+        session.add(club)
+
+club1 = "" # Barcelona
+club2 = "" # Independiente del Valle
+club3 = "" # Mushuc Runa
+club4 = "" # Universidad Católica
+
+for c in clubs:
+    if c.nombre == "Barcelona":
+        club1 = c
+    elif c.nombre == "Independiente del Valle":
+        club2 = c
+    elif c.nombre == "Mushuc Runa":
+        club3 = c
+    elif c.nombre == "Universidad Católica":
+        club4 = c
+
+# OBTENCION DE DATOS - CLUB
+with open('data/datos_jugadores.txt', 'r', encoding='utf-8') as data_jugadores:
+    for j in data_jugadores:
+        row_jugadores = j.strip().split(';') # Lectura de las lineas, seprandolas por ;
+        # Crear un objeto de tipo Jugador
+        # En los datos de los jugadores tenemos el club por su nombre
+        # y para añadirlos a la base de datos necesitamos el club como objeto
+        # Pasarle los parametros obtenidos
+        # como todos los datos son cadenas, el dorsal la transformamos a entero
+        jugador = Jugador(nombre=row_jugadores[3],dorsal=int(row_jugadores[2]),posicion=row_jugadores[1])
+        print(jugador)
+        session.add(jugador)
